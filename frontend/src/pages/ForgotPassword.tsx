ac48e013180f2d8c1cb01a1c67018318bd621300
malhaouit <abdelmounaim.abdel@gmail.com>
2024-09-09 02:09:29 +0100

    Remove unused App.css and index.css

---
+++ frontend/src/pages/ForgotPassword.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/ForgotPassword.css';
+
+function ForgotPassword() {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  const handleForgotPassword = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/auth/reset-password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),  // Send email to backend
+      });
+
+      const data = await response.json();
+
+      if (data.msg === 'Password reset email sent') {
+        alert('Reset link sent to your email.');
+	navigate('/');
+      } else {
+        alert('Email not found');
+      }
+    } catch (error) {
+      console.error('Error sending reset link:', error);
+      setMessage('Error: Unable to process request.');
+    }
+  };
+
+  return (
+    <div className="forgot-password-container">
+      <h1 className="forgot-password-title">Forgot Password</h1>
+      <input
+        type="email"
+        placeholder="Enter your email"
+	className="forgot-password-input"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button onClick={handleForgotPassword} className="forgot-password-button">
+	Send Reset Link
+      </button>
+    </div>
+  );
+}
+
+export default ForgotPassword;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import HomeHeader from '../components/HomeHeader/HomeHeader'; // Updated import
+
+function Home() {
+  return (
+    <div>
+      <HomeHeader /> {/* Display the HomeHeader at the top */}
+      {/* Add other components or content here */}
+      <h1>Welcome to Event Finder</h1>
+    </div>
+  );
+}
+
+export default Home;

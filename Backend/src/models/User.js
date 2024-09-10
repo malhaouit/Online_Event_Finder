530462e9470c161f13dc9f2fa5937755c1f25175
malhaouit <abdelmounaim.abdel@gmail.com>
2024-09-10 14:01:28 +0100

    Update the shema by adding two new files to comfirm registartion

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -5,7 +5,9 @@ const { isEmail } = require('validator');
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: [true, 'Email is required'], unique: true, lowercase: true, validate: [isEmail, 'Please enter a valid email']},
-    password: { type: String }
+    password: { type: String },
+    isEmailConfirmed: { type: Boolean, default: false },
+    confirmationToken: { type: String }
 });
 
 // Index definitions

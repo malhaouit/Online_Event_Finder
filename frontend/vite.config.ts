32588e3c6919c55913dba7cd28efb64d419d792e
malhaouit <abdelmounaim.abdel@gmail.com>
2024-09-13 15:44:33 +0100

    Update vite.config.ts and vercel.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Base set for root deployment
   build: {
-    outDir: 'dist',
+    outDir: 'dist',  // Ensure Vite outputs build files to 'dist'
   },
 });

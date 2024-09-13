ee6072b2c54536b14b657d6a6549502774137958
malhaouit <abdelmounaim.abdel@gmail.com>
2024-09-13 23:24:38 +0100

    Handle file size limit error in image upload

--- Backend/src/controllers/eventController.js
+++ Backend/src/controllers/eventController.js
@@ -40,8 +40,12 @@ exports.createEvent = (req, res) => {
     }
     upload(req, res, async (err) => {
         if (err) {
+	    if (err.code === 'LIMIT_FILE_SIZE') {
+		console.log('File upload error:', err);
+		return res.status(400).json({ msg: 'Image size should be less than 1MB.' });
+	    }
             console.error('File upload error:', err);
-            return res.status(400).json({ msg: err });
+            return res.status(400).json({ msg: 'File upload error.' });
         } else {
             const { title, description, date, time, location, image, capacity } = req.body;
 

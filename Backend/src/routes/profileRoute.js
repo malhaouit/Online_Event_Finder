4b48932292ee689f7d686322d0d2807076c052be
malhaouit <abdelmounaim.abdel@gmail.com>
2024-09-22 19:14:00 +0100

    Add route for fetching logged-in user's profile

--- Backend/src/routes/profileRoute.js
+++ Backend/src/routes/profileRoute.js
@@ -1,17 +1,15 @@
-// src/routes/profileRoute.js
-
 const express = require('express');
 const router = express.Router();
 const profileController = require('../controllers/profileController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Route to update the profile image
+// Route to fetch the logged-in user's profile (requires authentication)
+router.get('/me', authMiddleware('jwt'), profileController.getLoggedInUserProfile);
+
+// Route to fetch user profile data (public access)
+router.get('/:userId', profileController.getProfile);
+
+// Route to update the profile image (requires authentication)
 router.post('/update-profile-image', authMiddleware('jwt'), profileController.updateProfileImage);
-
-// Route to fetch the logged-in user's profile
-router.get('/me', authMiddleware('jwt'), profileController.getProfile);
-
-// Route to fetch any user's profile by their userId
-router.get('/:userId', authMiddleware('jwt'), profileController.getProfileById);
 
 module.exports = router;
